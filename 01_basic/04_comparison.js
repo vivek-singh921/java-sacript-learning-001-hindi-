c76419ae5bd132ae0bd611c29efc05616d3568d1
vivek-singh921 <142828904+vivek-singh921@users.noreply.github.com>
2026-06-19 14:03:26 +0000

    comparison

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0) // In comparisons null is treated as 0
+console.log(null == 0) // In equality check null is not treated as 0
+console.log(null >= 0)
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+// === => strict check (value + datatype)
+console.log("2" === 2);
+
+
+
+
+
+
+
