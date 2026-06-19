eaafbed7554f02d1969e9d4f0e8ad03cc9571a23
vivek-singh921 <142828904+vivek-singh921@users.noreply.github.com>
2026-06-19 13:43:10 +0000

    Operations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33abcd"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 // "33" => 33
 // "33abc" => NaN
@@ -14,7 +14,7 @@ console.log(valueInNumber)
 let isLoggedIn = "Singh"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 
 // 1 => true; 0 => false
@@ -25,5 +25,39 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
-console.log(stringNumber)
+// console.log(typeof stringNumber)
+// console.log(stringNumber)
+
+
+// *************************** Operations ********************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "Vivek"
+let str2 = " Singh"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "3")
+
+// console.log( (3 + 4) * 5 % 3)
+
+console.log(true)
+
+// let gameCounter = 100;
+// gameCounter++
+// ++gameCounter
+// console.log(gameCounter)
