51fb9749c29fac59f839ead91e9736f062c3fa29
vivek-singh921 <142828904+vivek-singh921@users.noreply.github.com>
2026-06-21 17:56:56 +0000

    nums math

---
+++ 01_basic/06_nums_and_math.js
@@ -0,0 +1,25 @@
+// const score = 400
+// console.log(score)
+
+// const balance = new Number(500)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+
+const otherNumber = 223.8466
+console.log(otherNumber.toPrecision(4))
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+// ++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++
+
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(ceil(4.2))
+console.log(Math.floor(4.2))
+console.log(Math.min(2,5,7,9,8))
+console.log(Math.max(2,5,7,9,8))
