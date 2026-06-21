f16abc985ebd217ee0453ec7ef31353317b97681
vivek-singh921 <142828904+vivek-singh921@users.noreply.github.com>
2026-06-21 17:08:55 +0000

    strings again

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,35 @@
+const name = "hitesh"
+const repoCount = 50
+
+//console.log(name + repoCount + " Some")
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// const gameName = new String('hiteshhc')
+
+//console.log(gameName[0])
+//console.log(gameName.__proto__);
+
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName)
+// console.log(gameName.charAt(3))
+
+// console.log(gameName.indexOf('h'))
+
+const gameName = new String('hitesh-hc')
+
+//const newString = gameName.substring(0,4)
+//console.log(newString)
+// const anotherString = gameName.slice(-9,4) 
+// console.log(anotherString)
+
+// const newString = "   Singh   "
+// console.log(newString)
+// console.log(newString.trim())
+
+const url = "https://hitesh.com/hitesh%20chaudhary"
+
+//console.log(url.replace('%20','-'))
+console.log(url.replace('com','in'))
