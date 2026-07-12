d2634d92eb49731783228b1b9c5da187c2342aba
vivek-singh921 <142828904+vivek-singh921@users.noreply.github.com>
2026-07-12 18:05:00 +0000

    arrays

---
+++ 02_basics/array.js
@@ -0,0 +1,41 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros =  ["shaktiman","naagraj"]
+
+
+// console.log(myArr[0])
+// console.log(myArr)
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+// console.log(myArr)
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(9))
+
+// const newArr = myArr.join()
+
+// console.log(myArr)
+// console.log(newArr)
+
+// console.log(typeof newArr)
+
+// slice, splice
+
+console.log("A ", myArr)
+
+const myn1 = myArr.slice(0,3)
+
+console.log(myn1)
+console.log("B ",myArr)
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2)
+console.log("c ",myArr)
